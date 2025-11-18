d553532b78142b8be11a4581d9448b6dee90bd52
Giovanna <giovanna.silva89@fatec.sp.gov.br>
2025-11-18 01:32:02 -0300

    adiciona ReceitaDto

---
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/dto/ReceitaDTO.java
@@ -0,0 +1,11 @@
+package com.seuprojeto.mopo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ReceitaDTO (
+    @NotBlank String nome,
+    @NotBlank String descricao,
+    @NotBlank String tempoPreparo,
+    @NotBlank String rendimento,
+    @NotBlank String dataInsercao
+){}
