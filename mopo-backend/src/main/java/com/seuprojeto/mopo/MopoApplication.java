1b99e792345512610a19f29277ba0866de8578a9
Guilherme Santana Pessa <103978528+armanli@users.noreply.github.com>
2025-11-19 22:22:14 -0300

    build: atualiza e adiciona dependências, versão do Spring e nome do projeto

--- mopo-backend/src/main/java/com/seuprojeto/mopo/MopoApplication.java
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/MopoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MopoBackendApplication {
+public class MopoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MopoBackendApplication.class, args);
+        SpringApplication.run(MopoApplication.class, args);
     }
 }
