a3d43ecd112d1e5102eaa905c13f04a75e8b7b3d
Guilherme <guilhermesantanapessa12@gmail.com>
2025-11-25 18:08:57 -0300

    refactor: renomeia service do cliente, variáveis e funções dentro da classe

--- mopo-backend/src/main/java/com/seuprojeto/mopo/controller/ClientController.java
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/controller/ClientController.java
@@ -2,11 +2,9 @@ package com.seuprojeto.mopo.controller;
 
 import com.seuprojeto.mopo.dto.ClienteDTO;
 import com.seuprojeto.mopo.model.Cliente;
-import com.seuprojeto.mopo.service.ClienteService;
-import jakarta.validation.Valid;
+import com.seuprojeto.mopo.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,15 +16,15 @@ import java.util.List;
 public class ClientController {
 
   @Autowired
-  private ClienteService service;
+  private ClientService service;
 
   @PostMapping
   public ResponseEntity<ClienteDTO> create(@RequestBody Cliente client) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(client));
+    return ResponseEntity.status(HttpStatus.CREATED).body(service.create(client));
   }
 
   @GetMapping
   public ResponseEntity<List<Cliente>> readAll() {
-    return ResponseEntity.status(HttpStatus.OK).body(service.listar());
+    return ResponseEntity.status(HttpStatus.OK).body(service.readAll());
   }
 }
---
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/service/ClientService.java
@@ -0,0 +1,25 @@
+package com.seuprojeto.mopo.service;
+
+import com.seuprojeto.mopo.dto.ClienteDTO;
+import com.seuprojeto.mopo.model.Cliente;
+import com.seuprojeto.mopo.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+  @Autowired
+  private ClienteRepository repository;
+
+  public ClienteDTO create(Cliente client) {
+    var entity = repository.save(client);
+    return new ClienteDTO(entity.getId(), entity.getNome(), entity.getEmail(), entity.getTelefone());
+  }
+
+  public List<Cliente> readAll() {
+    return repository.findAll();
+  }
+}
